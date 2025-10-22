6ad93cb42f4196e885b4377a75d3156a8c2047d9
David ROCHELET <drochelet@isagri.fr>
2025-10-22 20:07:45 +0200

    Add Vue.js script to index.html, style adjustments for logo in CSS, and update logo image format in Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,8 @@
   <header class="header">
     <div class="header-content">
       <router-link to="/" class="logo">
-        <img src="/images/logo-cpbo.svg" alt="Logo CPBO" class="logo-image" />
-        <span>CPBO</span>
+        <img src="/images/logo-cpbo.webp" alt="Logo CPBO" class="logo-image" />
+        <span><a id="logo" href="/">CPBO</a></span>
       </router-link>
       <Navigation />
     </div>
